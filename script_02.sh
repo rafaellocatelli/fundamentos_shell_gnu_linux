e378df83d5e69d091c854c94544b3a5f00c7ce39
rafaellocatelli <rafaelarseniolocatelli@gmail.com>
2024-02-11 17:12:31 -0300

    update

--- script_02.sh
+++ script_02.sh
@@ -2,11 +2,12 @@
 echo 'Primeiro argumento:   '   $1
 echo 'Todos argumentos:     '   $@
 echo ---------------------------------
-shift 3
-echo 'Primeiro argumento:   '   $1
-echo 'Todos argumentos:     '   $@
 echo ---------------------------------
 shift 1
 echo 'Primeiro argumento:   '   $1
 echo 'Todos argumentos:     '   $@
+shift 3
+echo 'Primeiro argumento:   '   $1
+echo 'Todos argumentos:     '   $@
+
 
